a751f19812f5d62f22c5eca7575d9697d1ab5c86
Pratik Narule <pratiknarule88@gmail.com>
2026-07-12 01:27:37 +0530

    Problems on LinkedList Serach and Occurence etc

---
+++ Programming/C_Programming/Competitive/41/Program3_41.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+#define True 0
+#define False 1
+typedef  int BOOL;
+
+// InsetAtFirst 
+struct node {
+    int data;
+    struct node * next;
+};
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void Display(PNODE first)
+{
+      while(first!=NULL)
+    {
+        printf("|%d|->",first->data);
+        first=first->next;
+    }
+    printf("NULL \n");
+   
+}
+int count (PNODE first)
+{
+  return 0;
+}
+
+void InsertFirst(PPNODE first,int iNo)
+{
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+
+    newn->data=iNo;
+    newn->next=NULL;
+
+    if(*first==NULL)// LinkedList is Empty
+    {
+        *first=newn;
+
+    }
+    else// LinkedList contain At least one Node 
+    {
+           newn->next=*first;
+           *first=newn;
+    }
+
+    
+
+}
+void InsertLast(PPNODE first,int iNo)
+{
+
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+
+    newn->data=iNo;
+   
+
+    if(*first==NULL)// LinkedList is Empty
+    {
+        *first=newn;
+
+    }
+    else// LinkedList contain At least one Node 
+    {
+        PNODE temp=*first;
+
+    }
+}
+
+
+void DisplayOdd(PNODE first)
+{
+    while(first!=NULL)
+    { 
+        if((first->data % 2!=0))
+        {
+            printf("|%d|",first->data);
+
+        }
+        first=first->next;
+    }
+    printf("\n");
+}
+
+int LastOccurence(PNODE first, int iNO)
+{
+    int iPost=0;
+    int iCount=0;
+    while(first !=NULL)
+    {
+        if(first->data==iNO)
+        {
+            iPost=iCount;
+           
+        }
+        first=first->next;
+        iCount++;
+
+    }
+   return iPost+1;
+}
+
+
+
+
+
+
+
+int main()
+{
+    PNODE head=NULL;
+
+    InsertFirst(&head,101);
+    InsertFirst(&head,51);
+    InsertFirst(&head,11);
+    InsertFirst(&head,22);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+    InsertFirst(&head,32);
+    InsertFirst(&head,21);
+    
+   
+    Display(head);
+     int iRet=0;
+     iRet=LastOccurence(head,11);
+     printf("Last Occur is at pos%d :\n",iRet);
+
+  
+
+
+
+   
+
+
+       
+
+    return 0;
+}
---
+++ Programming/C_Programming/Competitive/41/Program5_41.c
@@ -0,0 +1,143 @@
+
+#include <stdio.h>
+
+#include <stdlib.h>
+#define True 0
+#define False 1
+typedef  int BOOL;
+
+// InsetAtFirst 
+struct node {
+    int data;
+    struct node * next;
+};
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void Display(PNODE first)
+{
+      while(first!=NULL)
+    {
+        printf("|%d|->",first->data);
+        first=first->next;
+    }
+    printf("NULL \n");
+   
+}
+int count (PNODE first)
+{
+  return 0;
+}
+
+void InsertFirst(PPNODE first,int iNo)
+{
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+
+    newn->data=iNo;
+    newn->next=NULL;
+
+    if(*first==NULL)// LinkedList is Empty
+    {
+        *first=newn;
+
+    }
+    else// LinkedList contain At least one Node 
+    {
+           newn->next=*first;
+           *first=newn;
+    }
+
+    
+
+}
+void InsertLast(PPNODE first,int iNo)
+{
+
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+
+    newn->data=iNo;
+   
+
+    if(*first==NULL)// LinkedList is Empty
+    {
+        *first=newn;
+
+    }
+    else// LinkedList contain At least one Node 
+    {
+        PNODE temp=*first;
+
+    }
+}
+
+
+void DisplayOdd(PNODE first)
+{
+    while(first!=NULL)
+    { 
+        if((first->data % 2!=0))
+        {
+            printf("|%d|",first->data);
+
+        }
+        first=first->next;
+    }
+    printf("\n");
+}
+
+int CountLess(PNODE first, int X)
+{
+   
+    int iCount=0;
+    while(first !=NULL)
+    {
+        if((first->data)<X)
+        {
+            
+            iCount++;
+           
+        }
+        first=first->next;
+
+    }
+   return iCount;
+}
+
+
+
+
+
+
+
+int main()
+{
+    PNODE head=NULL;
+
+    InsertFirst(&head,101);
+    InsertFirst(&head,51);
+    InsertFirst(&head,11);
+    InsertFirst(&head,22);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+    InsertFirst(&head,32);
+    InsertFirst(&head,21);
+    
+    int ivalue=0;
+    printf("Enter the Number \n");
+    scanf("%d",&ivalue);
+   
+    Display(head);
+     int iRet=0;
+     iRet=CountLess(head,ivalue);
+     printf(" Less than Given Number Count of Nodes   is %d :\n",iRet);
+
+   
+
+
+       
+
+    return 0;
+}
