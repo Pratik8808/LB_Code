7bd0bf3a8e047d1c2f39af90f45489627218a0e4
Pratik Narule <pratiknarule88@gmail.com>
2026-07-21 18:32:52 +0530

    CheckSum file same or not

--- Programming/Python_Programming/Competitve/29/Program429.py
+++ Programming/Python_Programming/Competitve/29/Program429.py
@@ -1,24 +1,7 @@
 import os
 import sys
 
-def Countstr(Str):
-     fobj=open("Demo.txt","r+")
-     count=0
-     for i in fobj:
-         line=i.split()
-         for j in line:
-             if(j==Str):
-                 count=count+1
-        
-       
-     return count
-
-
 def main():
-    if(len(sys.argv)==2):
-
-        Ret=Countstr(sys.argv[1])
-        print(Ret)
-   
+    pass
 if __name__=="__main__":
     main()
---
+++ Programming/Python_Programming/Competitve/29/Program529.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import hashlib
+
+def CompareFile(FileOne ,FileTwo):
+    
+     fobj1=open(FileOne,"r+b")
+     fobj2=open(FileTwo,"r+b")
+     hobj1=hashlib.md5()
+     hobj2=hashlib.md5()
+     Buffer1=fobj1.read(1000)
+     Buffer2=fobj2.read(1000)
+     while(len(Buffer1)>0):
+         hobj1.update(Buffer1)
+         Buffer1=fobj1.read(1000)
+
+     while(len(Buffer2)>0):
+         hobj2.update(Buffer2)
+         Buffer2=fobj2.read(1000)
+     print(hobj1.hexdigest())
+     print(hobj2.hexdigest())
+
+     return (hobj1.hexdigest()==hobj2.hexdigest())
+
+
+     
+        
+       
+
+def main():
+    if(len(sys.argv)==3):
+
+        Ret=CompareFile(sys.argv[1],sys.argv[2])
+        if(Ret):
+            print("Same File ")
+   
+if __name__=="__main__":
+    main()
