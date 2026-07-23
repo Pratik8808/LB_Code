0e64ddc34f3ff82fe9463940458175d08015cd55
Pratik Narule <pratiknarule88@gmail.com>
2026-07-23 16:13:11 +0530

    Aritmatic oop

---
+++ Programming/Python_Programming/Competitve/27/Program327.py
@@ -0,0 +1,18 @@
+class Numbers:
+    def __init__(self,Value):
+        self.No1=Value
+    def chkPrime(self):
+        pass
+    def Perfect(self):
+        pass
+    def factors():
+        pass
+    def SumFactors():
+        pass
+
+def main():
+    pass
+
+if __name__=="__name__":
+    main()
+
